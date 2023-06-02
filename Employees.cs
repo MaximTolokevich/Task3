49235526d9b7e1c8d9ccc9f34d69d847a84baa73
MaximTolokevich <68742286+MaximTolokevich@users.noreply.github.com>
2023-06-02 15:36:05 +0300

    Completed task

--- Employees.cs
+++ Employees.cs
@@ -4,13 +4,13 @@ namespace Task2
 {
     public class Employees
     {
-        [JsonPropertyName ("Name")]
+
         public string Name { get; set; }
-        [JsonPropertyName("Age")]
+
         public int Age { get; set; }
-        [JsonPropertyName("Id")]
+ 
         public int Id { get; set; }
-        [JsonPropertyName("Car")]
+
         public Car Car { get; set; }
     }
 }
